671d34e09d1bbe30d02c5504f225db5449167a6b
camkurup <camkurup@hotmail.com>
2023-01-02 15:19:52 +0100

    Created Connectionstring

--- SvinefarmAPI/Data/DataAccess.cs
+++ SvinefarmAPI/Data/DataAccess.cs
@@ -2,5 +2,7 @@
 {
     public class DataAccess
     {
+        //this is my connection string
+        string conectionString = "Host=localhost;Username=postgres;Password=1505;Database=ThePigFarm";
     }
 }

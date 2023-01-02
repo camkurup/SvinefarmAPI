51e3654be9426b80187623e4c3a981bff04e5b7e
camkurup <camkurup@hotmail.com>
2023-01-02 14:34:45 +0100

    Models added

--- SvinefarmAPI/Model/LightLogModel.cs
+++ SvinefarmAPI/Model/LightLogModel.cs
@@ -2,6 +2,11 @@
 {
     public class LightLogModel
     {
-       
+        public int Id { get; set; }
+        public int LevelOfLight { get; set; }
+        public DateTime TimeOfLog { get; set; }
+        public int LightLevelInStable { get; set; }
+
+        public LightLogModel() { }
     }
 }
--- SvinefarmAPI/Model/TemperatureLogModel.cs
+++ SvinefarmAPI/Model/TemperatureLogModel.cs
@@ -2,5 +2,11 @@
 {
     public class TemperatureLogModel
     {
+        public int Id { get; set; }
+        public int Temperature { get; set; }
+        public DateTime TimeOfLog { get; set; }
+        public bool UVLightOn { get; set; }
+
+        public TemperatureLogModel() { }
     }
 }
